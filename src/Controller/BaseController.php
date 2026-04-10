836e0ff754b0995d8ccee000c2a9e9b9fdd5d326
darkskullz9 <darxkullz9@gmail.com>
2026-04-10 14:41:31 +0200

    display cards, add auth, login, registration

--- src/Controller/BaseController.php
+++ src/Controller/BaseController.php
@@ -5,11 +5,15 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use App\Repository\ProduitRepository;
 
 final class BaseController extends AbstractController {
     #[Route('/', name: 'app_home')]
-    
-    public function index(): Response {
-        return $this->render('base/index.html.twig', []);
+    public function index(ProduitRepository $produitRepository): Response {
+        $produits = $produitRepository -> findAll();
+
+        return $this->render('base/index.html.twig', [
+            'produits' => $produits
+        ]);
     }
 }
