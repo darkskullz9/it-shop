b42edf9aef41b29ced9bed72b9f1bcf3db906681
darkskullz9 <darxkullz9@gmail.com>
2026-04-22 03:47:27 +0200

    create view to lists favorites

--- src/Controller/FavoriController.php
+++ src/Controller/FavoriController.php
@@ -2,17 +2,39 @@
 
 namespace App\Controller;
 
+use App\Entity\Produit;
+
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 final class FavoriController extends AbstractController
 {
-    #[Route('/favori', name: 'app_favori')]
-    public function index(): Response
-    {
-        return $this->render('favori/index.html.twig', [
-            'controller_name' => 'FavoriController',
+    #[Route('/private-favori/{id}', name: 'app_favori')]
+    public function toggle(Produit $produit, EntityManagerInterface $em, Request $request): Response {
+        $referer = $request->headers->get('referer');
+        $u = $this->getUser();
+
+        if($u->getProduits()->contains($produit)) {
+            $u->removeProduit($produit);
+        } else {
+            $u->addProduit($produit);
+        }
+
+        $em->persist($u);
+        $em->flush();
+
+        return $this->redirect($referer ?? $this->generateUrl('app_home'));
+    }
+
+    #[Route('/private-liste-favoris', name: 'app_liste_favoris')]
+    public function listeFavoris(): Response {
+        $favoris = $this->getUser()->getProduits();
+
+        return $this->render('favori/liste-favoris.html.twig', [
+            'favoris' => $favoris,
         ]);
     }
 }
