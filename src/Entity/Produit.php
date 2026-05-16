0e9e43c6bd7605541e873493121342d2f911b474
Darkskullz9 <darxkullz9@gmail.com>
2026-05-16 11:49:41 +0200

    fix: update setDescription method to accept nullable string

--- src/Entity/Produit.php
+++ src/Entity/Produit.php
@@ -132,7 +132,7 @@ class Produit
         return $this->description;
     }
 
-    public function setDescription(string $description): static
+    public function setDescription(?string $description): static
     {
         $this->description = $description;
 
