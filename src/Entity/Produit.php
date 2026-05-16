e5d06325dea74001aa6344ef21a6585e404f08cd
Darkskullz9 <darxkullz9@gmail.com>
2026-05-16 11:26:09 +0200

    fix: initialize description property in Produit entity

--- src/Entity/Produit.php
+++ src/Entity/Produit.php
@@ -38,7 +38,7 @@ class Produit
     #[ORM\Column(length: 500)]
     #[Assert\NotBlank(message: 'The description cannot be empty.')]
     #[Assert\Length(min: 5, max: 500, minMessage: 'Minimum 5 characters.', maxMessage: 'Maximum 500 characters.')]
-    private ?string $description = null;
+    private ?string $description;
 
     /**
      * @var Collection<int, Add>
