0268b187e360ff84f6b276f8cf724f9fd2001180
Darkskullz9 <darxkullz9@gmail.com>
2026-05-30 23:36:07 +0200

    fix: correct namespace for UserController to App\Controller\Admin

--- src/Controller/Admin/UserController.php
+++ src/Controller/Admin/UserController.php
@@ -1,10 +1,12 @@
 <?php
 
-namespace App\Controller;
+namespace App\Controller\Admin;
+
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
 
 #[Route('/admin')]
 #[IsGranted('ROLE_ADMIN')]
